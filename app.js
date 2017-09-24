e00a4f87a89d8d1933a88e7e889cdfe965d00ee0
Holden Lee <holdenl@princeton.edu>
2017-09-23 23:00:42 -0700

    fix permissions problem

--- app.js
+++ app.js
@@ -31,11 +31,11 @@ var dbHost = process.env.DB_HOST || 'localhost'
 var dbPort = process.env.DB_PORT || 27017;
 var dbName = process.env.DB_NAME || 'node-login';
 
-var dbURL = 'mongodb://'+dbHost+':'+dbPort+'/'+dbName;
-if (app.get('env') == 'live'){
+//var dbURL = 'mongodb://'+dbHost+':'+dbPort+'/'+dbName;
+//if (app.get('env') == 'live'){
 // prepend url with authentication credentials // 
-	dbURL = 'mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@'+dbHost+':'+dbPort+'/'+dbName;
-}
+dbURL = 'mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@'+dbHost+':'+dbPort+'/'+dbName;
+//}
 
 app.use(session({
 	secret: 'faeb4453e5d14fe6f6d04637f78077c76c73d1b4',
@@ -45,6 +45,7 @@ app.use(session({
 	store: new MongoStore({ url: dbURL })
 	})
 );
+
 
 require('./app/server/routes')(app);
 
